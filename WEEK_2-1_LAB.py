fca916838d8ab4e73ee3684798249ba15d236807
jwilk01 <165970832+jwilk01@users.noreply.github.com>
2025-08-25 14:25:45 -0400

    Add files via upload

---
+++ WEEK_2-1_LAB.py
@@ -0,0 +1,26 @@
+"""
+AGE = 
+GPA =
+NAME =
+LIKES_PYTHON(?BOOL) = 
+FUTURE_JOB = 
+
+
+WHAT HAPPENS IF YOU ADD AN INT TO A FLOAT?
+CAN YOU MULTIPLY A STRING BY A MUMBER?
+WHAT ERROR DO YOU GET WHEN TRYING TO ADD A STRING TO A NUMBER?
+
+"""
+
+age = 36
+gpa = 3.5
+name = "Joseph Wilk"
+likes_python = True
+future_job = "Cryptography"
+
+print("Age: ", age, "   GPA: ", gpa, "   Likes Python?: ", likes_python, "   Future Job: ", future_job)
+print("age TYPE: ", type(age), "    gpa TYPE: ", type(gpa), "    likes_python TYPE: ", type(likes_python), "    future_job TYPE: ", type(future_job))
+
+
+name_2 = name * 3
+print(name_2)
